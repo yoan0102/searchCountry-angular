485ca17f3f1f2b877fed5a5f462bbbc7be107fc2
Yoan Estrada Blanco <yoanetsrada01@gamil.com>
2023-06-18 22:10:56 -0400

    feature: creating routes project

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    HomePageComponent,
+    AboutPageComponent,
+    SidebarComponent,
+    ContactPageComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  exports: [HomePageComponent, AboutPageComponent, SidebarComponent, ContactPageComponent]
+})
+export class SharedModule { }
